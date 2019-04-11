659adb222d678c61adb17229355135d951e7b2c4
MatiasEKrenz <matiaskrenz@gmail.com>
2019-04-10 23:18:03 -0300

    Añadidos nuevos metodos

--- src/Main.java
+++ src/Main.java
@@ -19,15 +19,23 @@ public class Main {
 
         apellidosL_G();
 
+        System.out.println("\nintegrantes con correo mayor a 15 caracteres");
+
+        longCorreo();
+
+        System.out.println("\nDos primeros integrantes");
+
+        dosPrimeros();
+
     }
 
     private static void agregarIntegrantes(){
 
         integrantes.add(new Integrante(0, "Matias", "Krenz", "matias.krenz@mercadolibre.com"));
-        integrantes.add(new Integrante(1, "Federico", "Torres", "fede.torres@mercadolibre.com"));
+        integrantes.add(new Integrante(1, "Federico", "Torres", "fede.torres_sapee@mercadolibre.com"));
         integrantes.add(new Integrante(2, "Rodrigo", "Licente", "rodri.vicente@mercadolibre.com"));
         integrantes.add(new Integrante(3, "Guillermo", "Gook", "gillermo.cook@mercadolibre.com"));
-        integrantes.add(new Integrante(4, "Marco", "Martinez", "marco.martinez@mercadolibre.com"));
+        integrantes.add(new Integrante(4, "Marco", "Martinez", "marco.martinez_sapeeee@mercadolibre.com"));
 
     }
 
@@ -49,4 +57,25 @@ public class Main {
                 .filter(s -> s.getApellido().startsWith("G"))
                 .forEach(i -> System.out.println(i.getNombre() + " " + i.getApellido()));
     }
+
+    private static void longCorreo(){
+
+        integrantes
+                .stream()
+                .filter(s -> s.getCorreo().length() > 32)
+                .forEach(i -> System.out.println(i.getNombre() + " " + i.getApellido()));
+    }
+
+    private static void dosPrimeros(){
+
+        int[] i = {0}; // no se puede usar un entero
+        integrantes
+                //.stream()
+                .forEach(value -> {
+                    if (i[0]++ < 2) {
+                        System.out.println(value.getNombre() + " " + value.getApellido());
+                    }
+        });
+    }
+
 }
