a116289bec565d8c5b0ae3e611a49c9e20e6c650
MatiasEKrenz <matiaskrenz@gmail.com>
2019-04-10 22:36:57 -0300

    Agregados metodos que imprimen todos los integrantes y metodo que imprime integrantes cuyos apellidos comiencen con L o G

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
 
 
@@ -10,19 +11,42 @@ public class Main {
 
         agregarIntegrantes();
 
-        for (Integrante i : integrantes) {
+        System.out.println("\nVamos a imprimir todos los integrantes");
 
-            System.out.println (i.getNombre());
-        }
+        todosLosIntegrantes();
+
+        System.out.println("\nApellidos con L o G");
+
+        apellidosL_G();
+
     }
 
     private static void agregarIntegrantes(){
 
         integrantes.add(new Integrante(0, "Matias", "Krenz", "matias.krenz@mercadolibre.com"));
         integrantes.add(new Integrante(1, "Federico", "Torres", "fede.torres@mercadolibre.com"));
-        integrantes.add(new Integrante(2, "Rodrigo", "Vicente", "rodri.vicente@mercadolibre.com"));
-        integrantes.add(new Integrante(3, "Guillermo", "Cook", "gillermo.cook@mercadolibre.com"));
+        integrantes.add(new Integrante(2, "Rodrigo", "Licente", "rodri.vicente@mercadolibre.com"));
+        integrantes.add(new Integrante(3, "Guillermo", "Gook", "gillermo.cook@mercadolibre.com"));
         integrantes.add(new Integrante(4, "Marco", "Martinez", "marco.martinez@mercadolibre.com"));
 
+    }
+
+    private static void todosLosIntegrantes(){
+
+        integrantes.forEach(n -> System.out.println(n.getNombre() + " " + n.getApellido()));
+        //ArrayList.stream(integrantes).forEach(num -> System.out.println(num));
+    }
+
+    private static void apellidosL_G(){
+
+        integrantes
+                .stream()
+                .filter(s -> s.getApellido().startsWith("L"))
+                .forEach(i -> System.out.println(i.getNombre() + " " + i.getApellido()));
+
+        integrantes
+                .stream()
+                .filter(s -> s.getApellido().startsWith("G"))
+                .forEach(i -> System.out.println(i.getNombre() + " " + i.getApellido()));
     }
 }
